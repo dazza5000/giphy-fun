9382ce90b4ff70c5b1ea59b1e7b5800988c5ae9a
Darran Kelinske <darran.kelinske@gmail.com>
2016-07-16 08:44:23 -0500

    before adding recyclerview

--- app/src/main/java/ly/generalassemb/giphy/GiphyApplication.java
+++ app/src/main/java/ly/generalassemb/giphy/GiphyApplication.java
@@ -2,6 +2,8 @@ package ly.generalassemb.giphy;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by darrankelinske on 7/15/16.
  */
@@ -10,6 +12,8 @@ public class GiphyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        LeakCanary.install(this);
 
     }
 }
--- app/src/main/java/ly/generalassemb/giphy/MainActivity.java
+++ app/src/main/java/ly/generalassemb/giphy/MainActivity.java
@@ -1,9 +1,14 @@
 package ly.generalassemb.giphy;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,14 +26,17 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import static com.squareup.haha.guava.base.Ascii.checkNotNull;
+
 public class MainActivity extends AppCompatActivity {
     public static final String ENDPOINT =
-            "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+            "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&rating=pg&limit=100";
 
     public final static String LOG_TAG = "MainActivity";
 
@@ -56,9 +64,9 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this,
                     giphyList.get(0).getGifUrl(), Toast.LENGTH_SHORT).show();
 
-            String gifUrl = giphyList.get(0).getGifUrl();
-            String gifUrl2 = giphyList.get(1).getGifUrl();
-            String gifUrl3 = giphyList.get(2).getGifUrl();
+            String gifUrl = giphyList.get(new Random().nextInt(giphyList.size())).getGifUrl();
+            String gifUrl2 = giphyList.get(new Random().nextInt(giphyList.size())).getGifUrl();
+            String gifUrl3 = giphyList.get(new Random().nextInt(giphyList.size())).getGifUrl();
 
             Log.d("TAG", "the gifurl is: " +gifUrl);
 
@@ -136,11 +144,48 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
+//        Subscription subscription = getImageNetworkCall()
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Observer<String>() {
+//                    @Override
+//                    public void onCompleted() {
+//                        // Update user interface if needed
+//                        Log.d("completed","yes");
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Log.d("error",e.getMessage());
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(String bitmap) {
+//                        Log.d("bit",bitmap);
+//
+//                        // Handle result of network request
+//                    }
+//                });
+
     }
+
+//    public Observable<String> getImageNetworkCall() {
+//        // Insert network call here!
+//        return Observable.just("we");
+//    }
+
+
+
 
     public interface GiphyListener {
         void onGifsLoaded(List<Giphy> giphyList);
     }
+
+
 
 
 
@@ -201,4 +246,102 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    private static class GifAdapter extends RecyclerView.Adapter<GifAdapter.ViewHolder> {
+
+        private List<Giphy> mGifs;
+        private GifItemListener mItemListener;
+        private Context context;
+
+        public GifAdapter(Context context, List<Giphy> giphies, GifItemListener itemListener) {
+            this.context = context;
+            setList(giphies);
+            mItemListener = itemListener;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View churchView = inflater.inflate(R.layout.gif_item, parent, false);
+
+            return new ViewHolder(churchView, mItemListener);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+            Giphy giphy = mGifs.get(position);
+
+            Glide.with(context).load(giphy.getGifUrl())
+                    .crossFade()
+                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(
+                                Exception e, String model, Target<GlideDrawable> target,
+                                boolean isFirstResource) {
+
+                            // do something
+                            Log.d(LOG_TAG,"onException");
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(
+                                GlideDrawable resource, String model, Target<GlideDrawable> target,
+                                boolean isFromMemoryCache, boolean isFirstResource) {
+
+                            // do something
+                            Log.d(LOG_TAG,"onResourceReady");
+                            return false;
+                        }
+                    })
+                    .into(viewHolder.gifImageView);
+        }
+
+        public void replaceData(List<Giphy> giphies) {
+            setList(giphies);
+            notifyDataSetChanged();
+        }
+
+        private void setList(List<Giphy> churches) {
+            mGifs = checkNotNull(churches);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGifs.size();
+        }
+
+        public Giphy getItem(int position) {
+            return mGifs.get(position);
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            private ImageView gifImageView;
+            private GifItemListener mItemListener;
+
+            public ViewHolder(View itemView, GifItemListener listener) {
+                super(itemView);
+                mItemListener = listener;
+                gifImageView = (ImageView) itemView.findViewById(R.id.gif_image_view);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                int position = getAdapterPosition();
+                Giphy giphy = getItem(position);
+                mItemListener.onGifClick(giphy);
+
+            }
+        }
+    }
+
+    public interface GifItemListener {
+
+        void onGifClick(Giphy clickedGif);
+    }
+
+
 }
